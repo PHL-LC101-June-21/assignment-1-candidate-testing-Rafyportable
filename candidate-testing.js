2ed1d6a99d84c1b93a7ba899026d7a3cb6a99aeb
Rafael Perez <36138019+Rafyportable@users.noreply.github.com>
2021-06-22 04:48:15 +0000

    Got rid of the extra input statement.

--- candidate-testing.js
+++ candidate-testing.js
@@ -21,7 +21,6 @@ let candidateAnswers = [];
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
 
-let input = require('readline-sync');
 candidateName = input.question("What is your name? ");
 
 }
